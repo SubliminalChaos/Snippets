1d01109647d6f127205b727482ab638daf6d2613
Rick Pendrick <me@waqe.me>
2025-03-29 05:36:04 -0500

    class_methods

---
+++ class_methods_demo.md
@@ -0,0 +1,9 @@
+Class Methods
+
+Methods that operate on the class itself
+Created using the @classmethod decorator
+First argument always a reference to the class
+    By convention, named cls
+Used for:
+    Manipulating data common across all instances
+    Factories
---
+++ class_methods_demo.py
@@ -0,0 +1,17 @@
+from class_methods_demo_class import Vehicle
+
+boat = Vehicle.water_vehicle('Minnow', (30, 40, 10))
+print(boat.name)
+print(boat.num_wheels)
+print(boat.volume())
+
+print()
+car = Vehicle.road_vehicle('Kitt', (4, 3, 1.5), 4)
+print(car.name)
+print(car.num_wheels)
+print(car.volume())
+
+print()
+print(f'{Vehicle.all_float(boat)}')
+print(f'{Vehicle.all_float(boat, boat)}')
+print(f'{Vehicle.all_float(boat, boat, car)}')
