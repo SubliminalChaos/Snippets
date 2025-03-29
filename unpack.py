d6fc9a4686ba62261e90f801085d461b4ca94639
Rick Pendrick <me@waqe.me>
2025-03-29 05:52:16 -0500

    up

---
+++ unpack.py
@@ -0,0 +1,9 @@
+def total(galleons, sickles, knuts):
+    return (galleons * 17 + sickles) * 29 + knuts
+
+
+coins1 = [100, 50, 25]
+coins2 = {'galleons': 100, 'sickles': 50, 'knuts': 25}
+
+print(total(*coins1), "Knuts")  # integers
+print(total(**coins2), "Knuts")  # named arguments
