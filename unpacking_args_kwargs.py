efc108067c52aa73b6bf1278516d164f34c30f9c
Rick Pendrick <me@waqe.me>
2025-03-29 06:27:22 -0500

    up

---
+++ unpacking_args_kwargs.py
@@ -0,0 +1,14 @@
+# variadic functions
+
+def foo(*args, **kwargs):
+    print(f'Positional: {args}')
+
+foo(100, 50, 25)
+foo(100, 50, 25, 1)
+foo(100)
+
+def bar(*args, **kwargs):
+    print(f'Named: {kwargs}')
+
+bar(galleons=100, sickles=50, knuts=25)
+bar(galleons=100, sickles=50, knuts=25, pennies=1)
