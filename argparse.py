c1e4461b8fbef26fc96eaee37315d25a3aabd7f0
Rick Pendrick <me@waqe.me>
2025-03-29 05:48:47 -0500

    up

--- argparse.py
+++ argparse.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Meow like a cat")
 parser.add_argument('-n', default=1, help='number of times to meow', type=int)
-parser.add_argument('-c', help='test argument', action='store_true')
+parser.add_argument('-c', help='test argument flag', action='store_true')
 args = parser.parse_args()
 
 for _ in range(args.n):
